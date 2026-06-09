bfbd66f031d95bb2bfad14804a16f9611160ca5f
Ricardo Miranda <Dev.rm.f@outlook.com>
2026-06-09 02:22:31 -0600

    no es mucho pero es trabajo honesto!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,6 @@
+const contenedor = document.querySelector("#stays-container")
+
+
 async function loadStays() {
     const response = await fetch('/data/stays.json');
     const stays = await response.json();
@@ -19,3 +22,40 @@ mostrar.addEventListener("click", (e) => {
     menu.classList.add("top-0");
 });
 
+async function cargartarjetas() {
+    const respuesta = await fetch('/src/data/stays.json')
+    const estancias = await respuesta.json()
+    contenedor.innerHTML="";
+    estancias.forEach((estancia) => {
+     const tarjetahtml = `
+        <div class="bg-white rounded-lg max-w-xl mx-auto my-4 ">
+            
+            <div class="text-sm text-gray-800 mb-3">
+                <!-- Reemplazamos src="/src/images/fin.jpg" por la variable de la foto -->
+                <img class="rounded-4xl" src="${estancia.photo}" alt="${estancia.title}">
+            </div>
+
+            <div class="pt-2 text-sm to-gray-500">
+                <div class="flex items-center justify-between">
+                    
+                    <!-- Reemplazamos "entire apartment . 2 beds" -->
+                    <span class="font-semibold text-gray-400"> ${estancia.type} . ${estancia.beds !== null ? estancia.beds + ' beds' : ''}</span>
+                    
+                    <!-- Reemplazamos "4.4" por el rating -->
+                    <i class="fa-solid fa-star star text-amber-500"><span class="font-light text-gray-400 score">${estancia.rating}</span></i>
+                </div>
+                
+                <span class="flex justify-between items-baseline gap-1 font-semibold text-gray-700">
+                    <!-- Reemplazamos el título fijo por la variable -->
+                    <h3 class="mt-1 text-gray-900 font-bold text-base tracking-tight">${estancia.title}</h3>
+                </span>
+            </div>
+
+        </div>
+        `; 
+
+        contenedor.innerHTML += tarjetahtml;
+        
+    });
+}
+    cargartarjetas();

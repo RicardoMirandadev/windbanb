1986b643f1c39b3f57cae42bee45e6f9f6058746
Ricardo Miranda <Dev.rm.f@outlook.com>
2026-06-12 00:56:16 -0600

    fix: corregir ruta fetch sin public

--- src/scripts/stays.js
+++ src/scripts/stays.js
@@ -1,5 +1,5 @@
 export async function loadStays() {
-    const response = await fetch('/public/data/stays.json');
+    const response = await fetch('/data/stays.json');
     const stays = await response.json();
     return stays;
 }
